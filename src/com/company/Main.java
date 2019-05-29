3357166e055c066af6d946b0ed312082a32eb2dd
chaikozova <r.chaikozova@gmail.com>
2019-05-29 13:48:14 +0300

    homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Figure[] figures = new Figure[5];
+        Figure[] figures = new Figure[6];
         figures[0] = new Circle(4.0);
         figures[1] = new Rectangle(3.0, 4.5);
         figures[2] = new Square(2.7);
         figures[3] = new Triangle(2.2,4.1,6.5);
-        figures[4] = new Rectangle(12.4, 9.8);
+        figures[4] = new Rectangle(12, 9.8);
+        figures[5] = new Triangle(13,9,5.5);
 
         for (int i = 0; i < figures.length ; i++) {
             System.out.println(i + " (" + figures[i].getName()
